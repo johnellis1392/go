0ce122f8b1a236b6018fb6bb4561e9ba9c68d5d7
John Ellis <jhelli@amazon.com>
2018-04-05 09:38:14 -0700

    Add example usage of github.com/google/gxui library

---
+++ src/johnellis1392/gxui_test/main.go
@@ -0,0 +1,108 @@
+package main
+
+// This library is a golang wrapper for OpenGL's full
+// libraries. Every version of OpenGL is available here.
+// "github.com/go-gl/gl"
+//
+// This is the library used to generate the Go->C
+// OpenGL bindings used by github.com/go-gl/gl above.
+// "github.com/go-gl/glow"
+
+import (
+	"time"
+
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/gxfont"
+	"github.com/google/gxui/math"
+	"github.com/google/gxui/samples/flags"
+)
+
+func appMain(driver gxui.Driver) {
+	theme := flags.CreateTheme(driver)
+
+	font, err := driver.CreateFont(gxfont.Default, 75)
+	if err != nil {
+		panic(err)
+	}
+
+	window := theme.CreateWindow(380, 100, "Hi")
+	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray50))
+
+	label := theme.CreateLabel()
+	label.SetFont(font)
+	label.SetText("Hello world")
+
+	window.AddChild(label)
+
+	ticker := time.NewTicker(time.Millisecond * 30)
+	go func() {
+		phase := float32(0)
+		for _ = range ticker.C {
+			c := gxui.Color{
+				R: 0.75 + 0.25*math.Cosf((phase+0.000)*math.TwoPi),
+				G: 0.75 + 0.25*math.Cosf((phase+0.333)*math.TwoPi),
+				B: 0.75 + 0.25*math.Cosf((phase+0.666)*math.TwoPi),
+				A: 0.50 + 0.50*math.Cosf(phase*10),
+			}
+			phase += 0.01
+			driver.Call(func() {
+				label.SetColor(c)
+			})
+		}
+	}()
+
+	window.OnClose(ticker.Stop)
+	window.OnClose(driver.Terminate)
+}
+
+func main() {
+	gl.StartDriver(appMain)
+}
+
+// import (
+// 	"fmt"
+// 	"syscall"
+// 	"unsafe"
+// )
+//
+// type (
+// 	HANDLE uintptr
+// 	HWND   HANDLE
+// )
+//
+// // Note: This may all be windows stuff
+// var (
+// 	// Library
+// 	libuser32 uintptr
+//
+// 	// Functions
+// 	setParent uintptr
+// )
+//
+// func MustLoadLibrary(libname string) uintptr {
+// 	return uintptr(0)
+// }
+//
+// func MustGetProcAddress(lib uintptr, fn string) uintptr {
+// 	return uintptr(0)
+// }
+//
+// func init() {
+// 	is64bit := unsafe.Sizeof(uintptr(0)) == 8
+// 	libuser32 = MustLoadLibrary("user32.dll")
+// 	setParent = MustGetProcAddress(libuser32, "SetParent")
+// }
+//
+// // SetParent - Copy of function from github.com/lxn/win
+// func SetParent(hWnd HWND, parentHWnd HWND) HWND {
+// 	ret, _, _ := syscall.Syscall(setParent, 2, uintptr(hWnd), uintptr(parentHWnd), 0)
+// 	return HWND(ret)
+// }
+//
+// func main() {
+// 	fmt.Println("Hello, World!")
+//
+// 	// var handle1 uintptr
+//
+// }
